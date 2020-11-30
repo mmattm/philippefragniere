e529b5a624ac340a6fe25e658ea5126aead4a259
Matthieu Minguet <minguet.mat@gmail.com>
2020-11-30 20:17:21 +0100

    Updates

---
+++ precache-manifest.9a87247adec946c472983b264aa108a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d48e789688ec954cabea4e1b8a5367",
+    "url": "/philippefragniere.ch/index.html"
+  },
+  {
+    "revision": "7c2d0bd357c06dfcaede",
+    "url": "/philippefragniere.ch/static/css/main.9b0c13dd.chunk.css"
+  },
+  {
+    "revision": "2969faaddc66766bb3a4",
+    "url": "/philippefragniere.ch/static/js/2.2d74439b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/philippefragniere.ch/static/js/2.2d74439b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2d0bd357c06dfcaede",
+    "url": "/philippefragniere.ch/static/js/main.86e82478.chunk.js"
+  },
+  {
+    "revision": "f9c400ebe539fc663465",
+    "url": "/philippefragniere.ch/static/js/runtime-main.7edfa5f4.js"
+  },
+  {
+    "revision": "525575d1010e6629689e3b2fd7d29dcd",
+    "url": "/philippefragniere.ch/static/media/GT-America-Standard-Regular.525575d1.eot"
+  },
+  {
+    "revision": "6677818f4f6a8ca945811ce7aa002b57",
+    "url": "/philippefragniere.ch/static/media/GT-America-Standard-Regular.6677818f.woff2"
+  },
+  {
+    "revision": "d331aa7aa4edc77bb61ec9c412cb4480",
+    "url": "/philippefragniere.ch/static/media/GT-America-Standard-Regular.d331aa7a.woff"
+  }
+]);
