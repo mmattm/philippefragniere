d14f7f2f3951a14910171ead9a1cb5b004caa3d8
Matthieu Minguet <minguet.mat@gmail.com>
2020-11-30 20:52:57 +0100

    Updates

---
+++ precache-manifest.772b2b8b976d1f22712d5e00b8de87ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552ae6028ba043fa2f8ad3778c722253",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be0caf41e22cf6b12713",
+    "url": "./static/css/main.8a5df836.chunk.css"
+  },
+  {
+    "revision": "2969faaddc66766bb3a4",
+    "url": "./static/js/2.2d74439b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2d74439b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0caf41e22cf6b12713",
+    "url": "./static/js/main.563e6d18.chunk.js"
+  },
+  {
+    "revision": "3727c5e69b0d64b354aa",
+    "url": "./static/js/runtime-main.8b5f1bcc.js"
+  },
+  {
+    "revision": "525575d1010e6629689e3b2fd7d29dcd",
+    "url": "./static/media/GT-America-Standard-Regular.525575d1.eot"
+  },
+  {
+    "revision": "6677818f4f6a8ca945811ce7aa002b57",
+    "url": "./static/media/GT-America-Standard-Regular.6677818f.woff2"
+  },
+  {
+    "revision": "d331aa7aa4edc77bb61ec9c412cb4480",
+    "url": "./static/media/GT-America-Standard-Regular.d331aa7a.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/www.philippefragniere.ch/precache-manifest.f680e087109de995da16a635b33d1f49.js"
+  "./precache-manifest.772b2b8b976d1f22712d5e00b8de87ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.philippefragniere.ch/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
